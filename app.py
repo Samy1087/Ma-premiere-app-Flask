023f43a0fc289bfb3e9507355ce8adaa1243d704
Samy1087 <mergane1290@gmail.com>
2025-09-03 21:23:34 +0000

    Ajout de fonctions : date_heure

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
-@app.route("/<prenom>")
-def bonjour(prenom):
-    """Cette route acceuille l'utilisateur"""
-    return f"<p>Bienvenue {prenom} sur mon site !</p>"
+@app.route("/")
+def bonjour():
+    """Cette route accueille l'utilisateur"""
+    prenom = "Ndeye"
+    return render_template("index.html", prenom=prenom)
 
 @app.route("/<int:a>/<int:b>")
 def calcul(a, b):
     """Cette route fais les calculs."""
     return f"{a} + {b} = {a+b}"
+
+@app.route("/date_heure")
+def heure():
+    date = datetime.datetime.now()
+    heure = date.hour
+    minute = date.minute
+    seconde = date.second
+    return render_template("date_heure.html", heure=heure, minute=minute, seconde=seconde)
 
 # Point d'entrée du programme.
 
